eb4aed1377704bf69771042ef89d0b07271a7a26
pavel <bikovsky.pavel@yandex.ru>
2020-06-01 17:46:49 +0300

    prime library

---
+++ default.nix
@@ -0,0 +1,21 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+
+  name = "libprime-0.1";
+
+  src = builtins.path {path = ./.;};
+
+  buildPhase = ''
+    gcc -fPIC -c sources/prime.c
+    gcc -shared -o libprime.so prime.o headers/prime.h
+  '';
+
+  installPhase = ''
+    mkdir -p $out/lib
+    mkdir -p $out/include
+
+    cp libprime.so $out/lib
+    cp headers/prime.h     $out/include
+  '';
+}
